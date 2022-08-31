8dbaab47a957064a1aaf7128dc6b110c252c8e87
Litvak Lev <sn.delui@gmail.com>
2022-08-31 18:57:24 +0500

    Displays mapped resource on successful resource loading

--- EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
+++ EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
@@ -8,8 +8,15 @@
 
 import Foundation
 
+public protocol ResourceView {
+    func display(_ viewModel: String)
+}
+
 public final class LoadResourcePresenter {
-    private let feedView: FeedView
+    public typealias Mapper = (String) -> String
+    
+    private let mapper: Mapper
+    private let resourceView: ResourceView
     private let loadingView: FeedLoadingView
     private let errorView: FeedErrorView
     
@@ -20,10 +27,11 @@ public final class LoadResourcePresenter {
                 comment: "Error message displayed when we can't load the image feed from the server")
     }
     
-    public init(feedView: FeedView, loadingView: FeedLoadingView, errorView: FeedErrorView) {
-        self.feedView = feedView
+    public init(resourceView: ResourceView, loadingView: FeedLoadingView, errorView: FeedErrorView, mapper: @escaping Mapper) {
+        self.resourceView = resourceView
         self.loadingView = loadingView
         self.errorView = errorView
+        self.mapper = mapper
     }
     
     public func didStartLoading() {
@@ -31,8 +39,8 @@ public final class LoadResourcePresenter {
         loadingView.display(FeedLoadingViewModel(isLoading: true))
     }
     
-    public func didFinishLoadingFeed(with feed: [FeedImage]) {
-        feedView.display(FeedViewModel(feed: feed))
+    public func didFinishLoading(with resource: String) {
+        resourceView.display(mapper(resource))
         loadingView.display(FeedLoadingViewModel(isLoading: false))
     }
     
