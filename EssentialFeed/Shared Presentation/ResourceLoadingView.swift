86986f4c01162215413f88b64464a209dac6a087
Litvak Lev <sn.delui@gmail.com>
2022-09-01 10:06:54 +0500

    Rename and move 'ResourceLoadingView' and 'ResourceLoadingViewModel' to a shared scope

---
+++ EssentialFeed/Shared Presentation/ResourceLoadingView.swift
@@ -0,0 +1,11 @@
+//
+//  ResourceLoadingView.swift
+//  EssentialFeed
+//
+//  Created by Lev Litvak on 01.09.2022.
+//  Copyright © 2022 Essential Developer. All rights reserved.
+//
+
+public protocol ResourceLoadingView {
+    func display(_ viewModel: ResourceLoadingViewModel)
+}
