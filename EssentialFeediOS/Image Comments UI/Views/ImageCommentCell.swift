4c9830bce479cc3db27d3a521b0872a0c83e0e57
Litvak Lev <sn.delui@gmail.com>
2022-09-02 18:26:20 +0500

    Implement image comments UI

---
+++ EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCommentCell.swift
+//  EssentialFeediOS
+//
+//  Created by Lev Litvak on 02.09.2022.
+//  Copyright © 2022 Essential Developer. All rights reserved.
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet private(set) public var messageLabel: UILabel!
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+}
---
+++ EssentialFeediOSTests/Image Comments UI/ImageCommentsSnapshotTests.swift
@@ -0,0 +1,53 @@
+//
+//  ImageCommentsSnapshotTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Lev Litvak on 02.09.2022.
+//  Copyright © 2022 Essential Developer. All rights reserved.
+//
+
+import XCTest
+import EssentialFeediOS
+import EssentialFeed
+
+class ImageCommentsSnapshotTests: XCTestCase {
+    func test_listWithComments() {
+        let sut = makeSUT()
+        
+        sut.display(comments())
+
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "IMAGE_COMMENTS_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "IMAGE_COMMENTS_dark")
+    }
+    
+    // MARK: - Helpers
+    private func makeSUT() -> ListViewController {
+        let bundle = Bundle(for: ListViewController.self)
+        let storyboard = UIStoryboard(name: "ImageComments", bundle: bundle)
+        let controller = storyboard.instantiateInitialViewController() as! ListViewController
+        controller.loadViewIfNeeded()
+        controller.tableView.showsVerticalScrollIndicator = false
+        controller.tableView.showsHorizontalScrollIndicator = false
+        return controller
+    }
+    
+    private func comments() -> [CellController] {
+        return [
+            ImageCommentCellController(model: ImageCommentViewModel(
+                message: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
+                date: "1000 years ago",
+                username: "A long long long very very long username")
+            ),
+            ImageCommentCellController(model: ImageCommentViewModel(
+                message: "The East Side Gallery is an open-air gallery in Berlin.",
+                date: "10 days ago",
+                username: "A username")
+            ),
+            ImageCommentCellController(model: ImageCommentViewModel(
+                message: "The East.",
+                date: "1 hour ago",
+                username: "A")
+            )
+        ]
+    }
+}
