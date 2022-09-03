ad533bef8284c635d292d5e8daa943d3ddbff34d
Litvak Lev <sn.delui@gmail.com>
2022-09-03 15:11:29 +0500

    Configure 'ErrorView' programmatically, so we don't duplicate layout logic in storyboards

--- EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
+++ EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
@@ -7,25 +7,48 @@ import EssentialFeed
 
 public final class ListViewController: UITableViewController, UITableViewDataSourcePrefetching, ResourceLoadingView, ResourceErrorView {
     private var loadingControllers = [IndexPath: CellController]()
-	@IBOutlet private(set) public var errorView: ErrorView?
+	private(set) public var errorView = ErrorView()
 
 	private var tableModel = [CellController]() {
 		didSet { tableView.reloadData() }
 	}
 
-	public override func viewDidLoad() {
-		super.viewDidLoad()
-		
-		refresh()
-	}
+    public var onRefresh: (() -> Void)?
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureErrorView()
+        refresh()
+    }
+    
+    private func configureErrorView() {
+        let container = UIView()
+        container.backgroundColor = .clear
+        container.addSubview(errorView)
+        
+        errorView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            errorView.leadingAnchor.constraint(equalTo: container.leadingAnchor),
+            errorView.trailingAnchor.constraint(equalTo: container.trailingAnchor),
+            errorView.topAnchor.constraint(equalTo: container.topAnchor),
+            errorView.bottomAnchor.constraint(equalTo: container.bottomAnchor)
+        ])
+        
+        tableView.tableHeaderView = container
+        
+        errorView.onHide = { [weak self] in
+            self?.tableView.beginUpdates()
+            self?.tableView.sizeTableHeaderToFit()
+            self?.tableView.endUpdates()
+        }
+    }
     
     public override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
         tableView.sizeTableHeaderToFit()
     }
-    
-    public var onRefresh: (() -> Void)?
 	
 	@IBAction private func refresh() {
 		onRefresh?()
@@ -41,7 +64,7 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
 	}
 	
 	public func display(_ viewModel: ResourceErrorViewModel) {
-		errorView?.message = viewModel.message
+		errorView.message = viewModel.message
 	}
 
 	public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
