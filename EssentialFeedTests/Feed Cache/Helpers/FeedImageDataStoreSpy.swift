0f16f432ce0c2674ceb9223f63357e1f3bba19a5
Litvak Lev <sn.delui@gmail.com>
2022-09-09 15:42:11 +0500

    Load images synchronously

--- EssentialFeedTests/Feed Cache/Helpers/FeedImageDataStoreSpy.swift
+++ EssentialFeedTests/Feed Cache/Helpers/FeedImageDataStoreSpy.swift
@@ -6,35 +6,34 @@ import Foundation
 import EssentialFeed
 
 class FeedImageDataStoreSpy: FeedImageDataStore {
-    func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {
-        
-    }
-    
-	enum Message: Equatable {
+    func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {}
+    func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {}
+	
+    enum Message: Equatable {
 		case insert(data: Data, for: URL)
 		case retrieve(dataFor: URL)
 	}
 	
 	private(set) var receivedMessages = [Message]()
-	private var retrievalCompletions = [(FeedImageDataStore.RetrievalResult) -> Void]()
+    private var retrievalResult: Result<Data?, Error>?
     private var insertionResult: Result<Void, Error>?
 
     func insert(_ data: Data, for url: URL) throws {
         receivedMessages.append(.insert(data: data, for: url))
         try insertionResult?.get()
 	}
-	
-	func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
-		receivedMessages.append(.retrieve(dataFor: url))
-		retrievalCompletions.append(completion)
-	}
-	
+    
+    func retrieve(dataForURL url: URL) throws -> Data? {
+        receivedMessages.append(.retrieve(dataFor: url))
+        return try retrievalResult?.get()
+    }
+    
 	func completeRetrieval(with error: Error, at index: Int = 0) {
-		retrievalCompletions[index](.failure(error))
+        retrievalResult = .failure(error)
 	}
 	
 	func completeRetrieval(with data: Data?, at index: Int = 0) {
-		retrievalCompletions[index](.success(data))
+		retrievalResult = .success(data)
 	}
 	
 	func completeInsertion(with error: Error, at index: Int = 0) {
